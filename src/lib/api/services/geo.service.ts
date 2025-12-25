a3c7920c67cc7c724813cf75b4310c980701a0ea
An <140888823+ancJ29@users.noreply.github.com>
2025-12-25 12:33:39 +0700

    sync@20251225.1233

--- src/lib/api/services/geo.service.ts
+++ src/lib/api/services/geo.service.ts
@@ -5,34 +5,46 @@ export class GeoApi {
   ): Promise<{
     oneLineAddress: string;
   }> {
-    const url = 'https://c-workers-m8ge9.api-bridge.work';
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({
-        handler: 'geo-data',
-        payload: {
-          longitude,
-          latitude,
-        },
-      }),
-    });
-    const long = longitude.toFixed(4);
-    const lat = latitude.toFixed(4);
-    let oneLineAddress = '';
-    const cachedKey = `__geo_data_${long}_${lat}`.replaceAll('.', '_');
-    if (localStorage.getItem(cachedKey)) {
-      console.log('get from cache:', cachedKey);
-      oneLineAddress = JSON.parse(localStorage.getItem(cachedKey) ?? '{}') as string;
-    } else {
-      const data = (await response.json()) as unknown as {
-        oneLineAddress: string;
+    const debug = true;
+    if (debug) {
+      return {
+        oneLineAddress: 'test',
       };
-      oneLineAddress = data.oneLineAddress;
-      localStorage.setItem(cachedKey, JSON.stringify(oneLineAddress));
     }
-    return {
-      oneLineAddress: oneLineAddress ?? '',
-    };
+    const url = 'https://c-workers-m8ge9.api-bridge.work';
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          handler: 'geo-data',
+          payload: {
+            longitude,
+            latitude,
+          },
+        }),
+      });
+      const long = longitude.toFixed(4);
+      const lat = latitude.toFixed(4);
+      let oneLineAddress = '';
+      const cachedKey = `__geo_data_${long}_${lat}`.replaceAll('.', '_');
+      if (localStorage.getItem(cachedKey)) {
+        console.log('get from cache:', cachedKey);
+        oneLineAddress = JSON.parse(localStorage.getItem(cachedKey) ?? '{}') as string;
+      } else {
+        const data = (await response.json()) as unknown as {
+          oneLineAddress: string;
+        };
+        oneLineAddress = data.oneLineAddress;
+        localStorage.setItem(cachedKey, JSON.stringify(oneLineAddress));
+      }
+      return {
+        oneLineAddress: oneLineAddress ?? '',
+      };
+    } catch (error) {
+      return {
+        oneLineAddress: '',
+      }
+    }
   }
 }
 
