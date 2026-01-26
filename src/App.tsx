b52aa250061a6a1647931f65335d6b9f118a6e38
An <140888823+ancJ29@users.noreply.github.com>
2026-01-26 17:44:40 +0700

    sync@20260126.1744

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,13 @@ import { useClientBranding } from '@/hooks/useClientBranding';
 import { usePWA } from '@/hooks/usePWA';
 import { useTranslation } from '@/hooks/useTranslation';
 import { router } from '@/routers';
-import { useEffect } from 'react';
 
 function App() {
   usePWA();
+
   useClientBranding();
+
   const { currentLanguage } = useTranslation();
-  useEffect(() => {
-    test();
-  }, []);
 
   return (
     <ErrorBoundary>
@@ -34,16 +32,3 @@ function App() {
 }
 
 export default App;
-
-
-function test() {
-  fetch('https://c-storage-hy4eq.api-bridge.work/3lzu/ghf8/3h0x0', {
-    method: 'POST',
-    headers: {
-      'x-token': 'kiw8isd1get',
-      'x-internal-access-key': 'ri5sta1alp2xlv14zf7b',
-    },
-  }).catch(_error => {
-    // console.error(error);
-  });
-}
